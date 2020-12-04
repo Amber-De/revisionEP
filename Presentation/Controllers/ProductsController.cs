387634b0ab29393b9409179b0e5990cd47c827ef
AMBER DEBONO <amber.debono.e24604@mcast.edu.mt>
2020-12-04 12:45:59 +0000

    File Upload

--- Presentation/Controllers/ProductsController.cs
+++ Presentation/Controllers/ProductsController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using ShoppingCart.Application.Services;
 using ShoppingCart.Application.ViewModels;
 using ShoppingCart.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Presentation.Controllers
 {
@@ -13,12 +15,14 @@ namespace Presentation.Controllers
     {
         private IProductsService _productsService;
         private ICategoriesService _categoriesService;
+        private IWebHostEnvironment _env;
 
         //Adding an instance in order to load categories
-        public ProductsController(IProductsService productsService, ICategoriesService categoriesService)
+        public ProductsController(IProductsService productsService, ICategoriesService categoriesService, IWebHostEnvironment env)
         {
             _productsService = productsService;
             _categoriesService = categoriesService;
+            _env = env;
         }
 
         /// <summary>
@@ -53,12 +57,28 @@ namespace Presentation.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(ProductViewModel data)
+        public IActionResult Create(ProductViewModel data, IFormFile file)
         {
             //this method is after the user clicks add or submit
             //Validation such as if the email is good, category wise, price is not below 0 or with (-).
             try
             {
+                if(file != null)
+                {
+                    //image file names should be unique.
+                    string newFilename = Guid.NewGuid() + System.IO.Path.GetExtension(file.FileName);
+                    string absolutePath = _env.WebRootPath + @"\Images\";
+
+                    //we are receiving file and copying it to a stream object
+
+                    using (var stream = System.IO.File.Create(absolutePath + newFilename))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    //this will go inside the database cause we don't put the full path, just the relative path
+                    data.ImageUrl = @"\Images\" + newFilename;
+                }
                 _productsService.AddProduct(data);
                 TempData["feedback"] = "Product was added successfully";
                 ModelState.Clear();
